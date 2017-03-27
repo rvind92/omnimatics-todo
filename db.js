0f3d3fab4cfbe1c5810c78693606d5bf1568a2a3
rvind92 <rvind92@gmail.com>
2017-03-27 22:31:40 +0800

    Removed extra local initialisation of db by sequelize.

--- db.js
+++ db.js
@@ -2,20 +2,16 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env === 'production') {
+if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: "postgres"
+		dialect: 'postgres'
 	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
 
 var db = {};
 
